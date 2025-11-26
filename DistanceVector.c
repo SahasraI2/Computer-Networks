d4801029309150e63174635f6d567ba752d951a2
SahasraI2 <153825867+SahasraI2@users.noreply.github.com>
2025-11-26 06:30:57 +0530

    Update DistanceVector.c

--- DistanceVector.c
+++ DistanceVector.c
@@ -1,74 +1,62 @@
 #include <stdio.h>
-#define MAX 10
+
 #define INF 99
+#define MAX 10
 
-int n;
+int main() {
+    int n, cost[MAX][MAX], dist[MAX][MAX], nextHop[MAX][MAX];
+    int i, j, k;
+    int updated;
 
-struct Router {
-    int dist[MAX];
-    char from[MAX];
-} routers[MAX];
+    printf("Enter number of routers: ");
+    scanf("%d", &n);
 
-void input() {
-    for (int i = 0; i < n; i++) {
-        printf("\nEnter distance from router %c to others (99 for no link):\n", 'A' + i);
-        for (int j = 0; j < n; j++) {
-            if (i == j) {
-                routers[i].dist[j] = 0;
-                routers[i].from[j] = 'A' + i;
-            } else {
-                printf("Distance from %c to %c: ", 'A' + i, 'A' + j);
-                scanf("%d", &routers[i].dist[j]);
-                if (routers[i].dist[j] != INF)
-                    routers[i].from[j] = 'A' + j;
-                else
-                    routers[i].from[j] = '-';
+    // Input cost matrix
+    for (i = 0; i < n; i++) {
+        printf("Enter costs from Router %c:\n", 'A' + i);
+        for (j = 0; j < n; j++) {
+            if (i == j) cost[i][j] = 0;
+            else {
+                printf("Cost to Router %c (99 if not adjacent): ", 'A' + j);
+                scanf("%d", &cost[i][j]);
             }
+            dist[i][j] = cost[i][j];
+            nextHop[i][j] = (cost[i][j] < INF && i != j) ? j : -1;
         }
     }
-}
 
-void build_table() {
-    int updated;
+    // Bellman-Ford relaxation until convergence
     do {
         updated = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (routers[i].dist[j] > routers[i].dist[k] + routers[k].dist[j]) {
-                        routers[i].dist[j] = routers[i].dist[k] + routers[k].dist[j];
-                        routers[i].from[j] = 'A' + k;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                for (k = 0; k < n; k++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        nextHop[i][j] = nextHop[i][k];
                         updated = 1;
                     }
                 }
             }
         }
     } while (updated);
-}
 
-void display() {
-    for (int i = 0; i < n; i++) {
-        printf("\nRouting Table for Router %c\n", 'A' + i);
-        printf("Destination : ");
-        for (int j = 0; j < n; j++)
-            printf("%c ", 'A' + j);
-        printf("\nNext Hop    : ");
-        for (int j = 0; j < n; j++)
-            printf("%c ", routers[i].from[j]);
-        printf("\nCost        : ");
-        for (int j = 0; j < n; j++)
-            printf("%d ", routers[i].dist[j]);
+    // Display routing tables
+    for (i = 0; i < n; i++) {
+        printf("\nRouting table for Router %c\n", 'A' + i);
+
+        printf("Destinations: ");
+        for (j = 0; j < n; j++) printf("%c ", 'A' + j);
+
+        printf("\nNext Hop   : ");
+        for (j = 0; j < n; j++)
+            printf("%c ", (nextHop[i][j] == -1) ? '-' : 'A' + nextHop[i][j]);
+
+        printf("\nCost       : ");
+        for (j = 0; j < n; j++) printf("%d ", dist[i][j]);
+
         printf("\n");
     }
-}
-
-int main() {
-    printf("Enter number of routers: ");
-    scanf("%d", &n);
-
-    input();
-    build_table();
-    display();
 
     return 0;
 }
